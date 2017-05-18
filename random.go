a67a8719323a546e77241bb03a41a7cc238271b9
Stanislav Vitko <vist.rcm@gmail.com>
2017-05-17 21:06:53 -0700

    move random to separate file for now

---
+++ random.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+// round float number to the nearest integer
+func round(f float64) int {
+	return int(math.Floor(f + .5))
+}
+
+// afterMinutes returns time after random number of minutes around 24 hours.
+// it is using normal distribution with mean = 24 and StdDev = 6. Assumes hours. Translate it to the minutes.
+func afterMinutes() time.Duration {
+	// Create and seed the generator.
+	// Typically a non-fixed seed should be used, such as time.Now().UnixNano().
+	// Using a fixed seed will produce the same output on every run.
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	desiredStdDev := 360.0 // manually tuned deviation parameter to make distribution wide enough
+	desiredMean := 1440.0  // 24 hours is 1440 minutes.
+
+	n := round(r.NormFloat64()*desiredStdDev + desiredMean) // calculate randomized time delay
+	return time.Duration(n) * time.Minute                   // return duration
+}
