0bc62f733c2f689f30d43e52c96182cd0c5e0287
Stanislav Vitko <vist.rcm@gmail.com>
2017-05-17 23:13:19 -0700

    move random to separate package to simplify life

--- randomizer/random.go
+++ randomizer/random.go
@@ -13,7 +13,7 @@ func round(f float64) int {
 
 // afterMinutes returns time after random number of minutes around 24 hours.
 // it is using normal distribution with mean = 24 and StdDev = 6. Assumes hours. Translate it to the minutes.
-func afterMinutes() time.Duration {
+func GetNextTime() time.Time {
 	// Create and seed the generator.
 	// Typically a non-fixed seed should be used, such as time.Now().UnixNano().
 	// Using a fixed seed will produce the same output on every run.
@@ -23,5 +23,5 @@ func afterMinutes() time.Duration {
 	desiredMean := 1440.0  // 24 hours is 1440 minutes.
 
 	n := round(r.NormFloat64()*desiredStdDev + desiredMean) // calculate randomized time delay
-	return time.Duration(n) * time.Minute                   // return duration
+	return time.Now().Add(time.Duration(n) * time.Minute)                   // return duration
 }
